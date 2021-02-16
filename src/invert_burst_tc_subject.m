47eddf0c187b5b9d83d93bf46eebe0a6035b8a04
James Bonaiuto <jbonaiuto@gmail.com>
2021-02-16 07:02:08 +0100

    refactoring

--- src/invert_burst_tc_subject.m
+++ src/invert_burst_tc_subject.m
@@ -27,20 +27,25 @@ if exist(params.plot_dir,'dir')~=7
     mkdir(params.plot_dir);
 end
 
+% Run EBB localizer
 pial_clusters=invert_burst_subject(subj_info,...
     'base_dir',params.base_dir,'surf_dir', params.surf_dir ,...
     'mri_dir', params.mri_dir,'patch_size', params.patch_size,...
     'data_type', params.data_type, 'plot_dir', params.plot_dir);
 
+% Load averaged burst file
 fname='mrcresp_TafdfC.mat';
 if strcmp(params.data_type,'evoked')
     fname='rcresp_TafdfC.mat';
 end
 data_file=fullfile(data_dir, fname);
 D=spm_eeg_load(data_file);
+
+% Get times and zero time
 times=D.time;
 zero_time=times(round(length(times)/2))+.5*(times(round(length(times)/2+1))-times(round(length(times)/2)));
 
+% Meshes to use
 subj_surf_dir=fullfile(params.surf_dir, sprintf('%s-synth',...
     subj_info.subj_id),'surf');
 pial_fname=fullfile(subj_surf_dir,'pial.ds.link_vector.gii');
@@ -51,25 +56,34 @@ wm_fname=fullfile(subj_surf_dir,'white.ds.link_vector.gii');
 mesh_fnames={wm_fname, pial_fname};
 mesh_names={'white','pial'};
 
-
-
+% For each cluster found by the localizer
 for cluster_idx=1:length(pial_clusters)
     cluster=pial_clusters(cluster_idx);
     max_vert=cluster.vertices(cluster.max_idx);
     
+    % Difference in link vector angle at max vertex of cluster and all
+    % other vertices in cluster
     norm_diffs=[];
     for i=1:length(cluster.vertices)
         norm_diffs(i)=atan2(norm(cross(pial_surf.normals(max_vert,:),pial_surf.normals(cluster.vertices(i),:))),...
             dot(pial_surf.normals(max_vert,:),pial_surf.normals(cluster.vertices(i),:)));
     end
+    % Invert at each vertex with link vector angle within .1 radians of the
+    % max vertex
     cluster.inv_verts=cluster.vertices(find(abs(norm_diffs<.1)));
 
+    % Run sliding window inversion at each selected vertex
     cluster.tc_fvals=zeros(length(cluster.inv_verts),length(mesh_names),length(times));
     for v_idx=1:length(cluster.inv_verts)
-        pial_max_vert=cluster.inv_verts(v_idx);
+        pial_prior=cluster.inv_verts(v_idx);
 
-        priors={pial_max_vert, pial_max_vert};
-        recompute_lgain=(v_idx==1);
+        % Because of our cool downsampling method, the vertices in the
+        % white matter and pial surface are cooresponding
+        priors={pial_prior, pial_prior};
+        % Only have to recompute the lead field gain matrix once
+        recompute_lgain=(cluster_idx==1) & (v_idx==1);
+        
+        % Run sliding window inversion for each mesh
         for m_idx=1:length(mesh_names)
             [cluster.tc_fvals(v_idx,m_idx,:),wois]=invert_sliding_window(subj_info,...
                 priors{m_idx}, mesh_names{m_idx}, mesh_fnames{m_idx},...
@@ -79,8 +93,11 @@ for cluster_idx=1:length(pial_clusters)
                 'win_overlap',params.win_overlap,...
                 'recompute_lgain',recompute_lgain);
         end
+        % Create centered WOI time
         centered_wois=wois-zero_time*1000;
+        % Figure out center of each sliding time window
         inner_times=centered_wois(:,1)+.5*(centered_wois(:,2)-centered_wois(:,1));
+        % If overlapping windows, cut off the edges
         if params.win_overlap
             left_idx=round((params.win_size-1)/2)+1;
             right_idx=length(times)-round((params.win_size-1)/2);
@@ -90,10 +107,9 @@ for cluster_idx=1:length(pial_clusters)
         end
     end
 
+    % Compute free energy difference
     cluster.f_diff=squeeze(cluster.tc_fvals(:,2,left_idx:right_idx)-cluster.tc_fvals(:,1,left_idx:right_idx));
     cluster.f_diff=reshape(cluster.f_diff,[size(cluster.tc_fvals,1) (right_idx-left_idx+1)]); 
-    base_fdiff=mean(cluster.f_diff(:,[1:3 end-2:end]),2);
-    cluster.bc_f_diff=cluster.f_diff-repmat(base_fdiff,1,size(cluster.f_diff,2));
     new_pial_clusters(cluster_idx)=cluster;
 end
 
@@ -107,18 +123,6 @@ invert_burst_tc_results.clusters=new_pial_clusters;
 save(fullfile(data_dir, 'invert_burst_tc_results.mat'), 'invert_burst_tc_results');
 
 fig=figure();
-subplot(2,1,1);
-hold all
-for cluster_idx=1:length(new_pial_clusters)
-    plot(inner_times(left_idx:right_idx),mean(new_pial_clusters(cluster_idx).bc_f_diff,1));
-end
-plot([inner_times(left_idx) inner_times(right_idx)],[0 0],'k');
-plot([inner_times(left_idx) inner_times(right_idx)],[3 3],'k--');
-plot([inner_times(left_idx) inner_times(right_idx)],[-3 -3],'k--');
-xlim([inner_times(left_idx) inner_times(right_idx)]);
-xlabel('Time (ms)')
-ylabel('BC Fpial-Fwhite');
-subplot(2,1,2);
 hold all
 for cluster_idx=1:length(new_pial_clusters)
     plot(inner_times(left_idx:right_idx),mean(new_pial_clusters(cluster_idx).f_diff,1));
